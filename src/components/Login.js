6abee6bb43e24a7ae3bb220418477649904b45ee
Tushar Taneja <tushartaneja24@gmail.com>
2023-07-04 22:56:26 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../styles/login.css";
+import { useNavigate, Link } from "react-router-dom";
+
+function Login() {
+  const history = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function submit(e) {
+    e.preventDefault();
+
+    try {
+      await axios
+        .post("http://localhost:8000/", {
+          email,
+          password,
+        })
+        .then((res) => {
+          if (res.data == "exist") {
+            history("/home", { state: { id: email } });
+          } else if (res.data == "notexist") {
+            alert("User have not sign up");
+          }
+        })
+        .catch((e) => {
+          alert("wrong details");
+          console.log(e);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="login">
+      <h1 style={{ color: "#45a049" }}>Welcome to SHIKSHAk!</h1>
+
+      <form action="POST" class="back">
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Password"
+        />
+        <input type="submit" onClick={submit} />
+        <button><Link to="/signup">Signup Page</Link></button>
+        
+      </form>
+      <br />
+
+      
+    </div>
+  );
+}
+
+export default Login;
